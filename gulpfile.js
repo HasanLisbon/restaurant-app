dc55d0e516a77df174998ab15e9b3ce8be49ba11
Stas <stas.webd@gmail.com>
2018-05-28 10:36:30 -0700

    optimization

--- gulpfile.js
+++ gulpfile.js
@@ -12,34 +12,115 @@ var webserver = require('gulp-webserver');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
 
-gulp.task('default', ['styles', 'lint'], function() {
+gulp.task('default', ['copy-html', 'copy-images', 'copy-manifest', 'styles', 'lint', 'copy-scripts'], function() {
   gulp.watch('sass/**/*.scss', ['styles']);
   gulp.watch('js/**/*.js', ['lint']);
+  gulp.watch('/*.html', ['copy-html']);
+  gulp.watch('./dist/*.html').on('change', browserSync.reload);
 
   browserSync.init({
-    server: './'
+    server: './dist'
   });
+});
+
+gulp.task('dist', [
+  'copy-html',
+  'images-process',
+  'copy-manifest',
+  'styles',
+  'lint',
+  'scripts-dist'
+]);
+
+gulp.task('copy-scripts', function() {
+  gulp.src('js/**/*.js')
+    .pipe(gulp.dest('dist/js'));
+  gulp.src('sw.js')
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('scripts-dist', function() {
+  gulp.src('js/**/*.js')
+    .pipe(babel({
+      presets: [
+        ['env', {
+          targets: {
+            browsers: ['last 2 versions']
+          }
+        }]
+      ]
+    }))
+    .pipe(uglify())
+    .on('error', function(err) {
+      console.log('[ERROR] '+ err.toString() );
+    })
+    .pipe(gulp.dest('dist/js'));
+  gulp.src('sw.js')
+    .pipe(babel({
+      presets: [
+        ['env', {
+          targets: {
+            browsers: ['last 2 versions']
+          }
+        }]
+      ]
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('copy-html', function() {
+  gulp.src('./*.html')
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('copy-images', function() {
+  gulp.src('img/*')
+    .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('copy-manifest', function() {
+  gulp.src('./manifest.json')
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('images-process', function() {
+  return gulp.src('img/*')
+    .pipe(imagemin({
+      progressive: true,
+      use: [pngquant()]
+    }))
+    .pipe(gulp.dest('dist/img'));
 });
 
 gulp.task('styles', function() {
   gulp.src('sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }).on('error', sass.logError))
     .pipe(autoprefixer({
       browsers: ['last 2 versions']
     }))
-    .pipe(gulp.dest('./css'))
-    .pipe(browserSync.stream());
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('lint', function () {
   return gulp.src(['js/**/*.js'])
-  // eslint() attaches the lint output to the eslint property
-  // of the file object so it can be used by other modules.
     .pipe(eslint())
-  // eslint.format() outputs the lint results to the console.
-  // Alternatively use eslint.formatEach() (see Docs).
     .pipe(eslint.format())
-  // To have the process exit with an error code (1) on
-  // lint error, return the stream and pipe to failOnError last.
     .pipe(eslint.failOnError());
+});
+
+gulp.task('test', ['dist']);
+
+gulp.task('webserver', function() {
+  gulp.src('dist')
+    .pipe(webserver({
+      host: 'localhost',
+      port: 8000,
+      livereload: true,
+      open: true,
+    }));
 });
--- sw.js
+++ sw.js
@@ -1,60 +1,60 @@
 var staticCacheName = 'restaurant-app-v3';
 var urlToCache = [
-    '/',
-    '/js/idb.js',
-    'restaurant.html',
-    '/js/dbhelper.js',
-    '/js/main.js',
-    '/js/restaurant_info.js',
-    'sw.js',
-    '/css/styles.css',
-    '/img/1.jpg',
-    '/img/2.jpg',
-    '/img/3.jpg',
-    '/img/4.jpg',
-    '/img/5.jpg',
-    '/img/6.jpg',
-    '/img/7.jpg',
-    '/img/8.jpg',
-    '/img/9.jpg',
-    '/img/10.jpg'
+  '/',
+  '/js/idb.js',
+  'restaurant.html',
+  '/js/dbhelper.js',
+  '/js/main.js',
+  '/js/restaurant_info.js',
+  'sw.js',
+  '/css/styles.css',
+  '/img/1.jpg',
+  '/img/2.jpg',
+  '/img/3.jpg',
+  '/img/4.jpg',
+  '/img/5.jpg',
+  '/img/6.jpg',
+  '/img/7.jpg',
+  '/img/8.jpg',
+  '/img/9.jpg',
+  '/img/10.jpg'
 ];
 
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open(staticCacheName).then(function(cache) {
-        return cache.addAll(urlToCache);
-      })
-    );
-  });
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.addAll(urlToCache);
+    })
+  );
+});
 
-  self.addEventListener('activate', function(event) {
-    event.waitUntil(
-      caches.keys().then(function(cacheNames) {
-        return Promise.all(
-          cacheNames.filter(function(cacheName) {
-            return cacheName.startsWith('restaurant-app-') &&
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('restaurant-app-') &&
                    cacheName != staticCacheName;
-          }).map(function(cacheName) {
-            return caches.delete(cacheName);
-          })
-        );
-      })
-    );
-  });
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
 
-  self.addEventListener('fetch', function(event) {
-      event.respondWith(
-          caches.match(event.request).then(function(response) {
-              if (response) return response;
-              return fetch(event.request);
-          })
-      )
-  });
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) return response;
+      return fetch(event.request);
+    })
+  );
+});
   
-  self.addEventListener('message', function(event) {
-    if (event.data.action === 'skipWaiting') {
-      self.skipWaiting();
-    }
-  });
+self.addEventListener('message', function(event) {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
   
